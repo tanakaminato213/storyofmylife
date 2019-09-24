4a3fc9efd7139e4239e66a7cdd7b5fe8ed59e25b
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-24 17:50:03 +0900

    commit

--- config/puma.rb
+++ config/puma.rb
@@ -18,16 +18,16 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# threads threads_count, threads_count
 
-preload_app!
+# preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+# rackup      DefaultRackup
+# port        ENV['PORT']     || 3000
+# environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection
+# end
