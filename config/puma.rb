cb39eb8fdfec97e9272955453ec800d863daa44c
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-24 18:28:56 +0900

    commit

--- config/puma.rb
+++ config/puma.rb
@@ -4,30 +4,30 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+# threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+# environment ENV.fetch("RAILS_ENV") { "development" }
 
 plugin :tmp_restart
 
 
-# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-# threads threads_count, threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-# preload_app!
+preload_app!
 
-# rackup      DefaultRackup
-# port        ENV['PORT']     || 3000
-# environment ENV['RACK_ENV'] || 'development'
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection
-# end
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
