e36850643f40c27e60ea0795e5bbfc453de27636
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-24 15:38:12 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'tanakaminato0213'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tanakaminato0213''
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tanakaminato0213'
 end
