187e5aea562b0322b6cf7f922a167f6df0eb3eeb
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-25 14:41:44 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'バケット名'
+    config.fog_directory  = 'tanakaminato0213'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
+      region: 'ap-northeast-1' ,
       path_style: true
     }
   else
