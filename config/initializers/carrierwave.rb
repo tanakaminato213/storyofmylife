3fe1d81dc8478a35e8fbb9306ffa7d67c17d7cb6
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-25 14:33:02 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,23 @@
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
-
+ 
 CarrierWave.configure do |config|
-  config.storage = :file
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
-  }
-    case Rails.env
-    when 'production'
-        config.fog_directory  = ENV['S3_BUCKET']
-        config.fog_directory  = 'tanakaminato0213'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tanakaminato0213'
-    end
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'バケット名'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
+      path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
+ 
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
