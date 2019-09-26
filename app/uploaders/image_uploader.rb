71bf7cb886cd4735de727a45770497a585ba63a7
tanakaminato213 <qwsazx012345@icloud.com>
2019-09-26 14:56:01 +0900

    commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,12 +6,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   process resize_to_fit: [400, 400]
   
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
+  if Rails.env.production?
     storage :fog
+  else
+    storage :file
   end
 
   # Override the directory where uploaded files will be stored.
